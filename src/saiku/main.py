d22a98ea27992fb14e058d420b478090be26be7f
Bruno Catão <brunogamacatao@gmail.com>
2018-05-29 17:46:28 -0300

    Added the render endpoint to the webservice

--- src/saiku/main.py
+++ src/saiku/main.py
@@ -5,17 +5,26 @@ Created on 2018/05/27
 '''
 
 from flask import Flask
+from flask import request
 from saiku.saiku_report_server import SaikuReportServer
 
 # Create Flask APP
 app = Flask(__name__)
 
+# Setup the webservice routes
 @app.route("/")
 def hello():
   return SaikuReportServer.getInstance().hello_world()
 
+@app.route("/render")
+def render():
+  reportId = request.args.get('id') or 'demo'
+  outputFormat = request.args.get('format') or 'pdf'
+  return SaikuReportServer.getInstance().render(reportId, outputFormat, {})
+
+# Start the development server
 if __name__ == '__main__':
   try:
-    app.run(port='5002')
+    app.run(port='5002', threaded=False)
   finally:
     SaikuReportServer.getInstance().stop()
